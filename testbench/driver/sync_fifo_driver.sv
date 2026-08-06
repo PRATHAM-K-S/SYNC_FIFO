3aa75b5ff1b24dac1f042fb656202d9e47945cb9
Pratham K S <prathamks2302@gmail.com>
2026-08-06 11:46:23 +0530

    testbench(driver): added sync_fifo_driver

---
+++ testbench/driver/sync_fifo_driver.sv
@@ -0,0 +1,11 @@
+class sync_fifo_driver extends uvm_driver;
+
+    // Factory registration
+    `uvm_component_utils(sync_fifo_driver)
+
+    // Class constructor
+    function new(string name="sync_fifo_driver", uvm_component parent);
+        super.new(name, parent);
+    endfunction
+    
+endclass
