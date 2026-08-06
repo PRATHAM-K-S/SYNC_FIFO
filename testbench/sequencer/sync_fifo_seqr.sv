f406431c4fcab80c980c9b3e29cce65f3bd0b101
Pratham K S <prathamks2302@gmail.com>
2026-08-06 13:03:55 +0530

    testbench(sequencer): added sync_fifo_seqr.sv

---
+++ testbench/sequencer/sync_fifo_seqr.sv
@@ -0,0 +1,11 @@
+class sync_fifo_seqr extends uvm_sequencer #(sync_fifo_seq_item);
+    
+    // Factory registration
+    `uvm_component_utils(sync_fifo_seqr)
+
+    // Class constructor    
+    function new(string name="sync_fifo_seqr", uvm_component parent);
+        super.new(name, parent)
+    endfunction
+
+endclass //sync_fifo_seqr extends uvm_sequencer #(sync_fifo_seq_item)
