00c0d1d76a6fe5662371995c2b7ed0cb4f4c338f
Pratham K S <prathamks2302@gmail.com>
2026-08-07 00:23:47 +0530

    testbench(package): added sync_fifo_pkg.sv

---
+++ testbench/package/sync_fifo_pkg.sv
@@ -0,0 +1,15 @@
+package sync_fifo_pkg;
+
+    import uvm_pkg::*;
+    `include "uvm_macros.svh"
+
+    `include "../sequence/sync_fifo_seq_item.sv"
+    `include "../driver/sync_fifo_drv.sv"
+    `include "../monitor/sync_fifo_inp_mon.sv"
+    `include "../monitor/sync_fifo_out_mon.sv"
+    `include "../sequencer/sync_fifo_seqr.sv"
+    `include "../agent/sync_fifo_agt.sv"
+    `include "../environment/sync_fifo_env.sv"
+    `include "../test/sync_fifo_test.sv"
+    
+endpackage
