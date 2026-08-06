301cca527acb7412ec5d9a256b663ccb2e397b4c
Pratham K S <prathamks2302@gmail.com>
2026-08-06 11:32:25 +0530

    testbench(sequence): added sync_fifo_seq_item.sv

---
+++ testbench/sequence/sync_fifo_seq_item.sv
@@ -0,0 +1,23 @@
+class sync_fifo_seq_item extends uvm_sequence_item;
+
+    // Factory registration
+    `uvm_object_utils(sync_fifo_seq_item)
+
+    // Input signals
+    rand bit wr_cs;
+    rand bit rd_cs;
+    rand bit wr_en;
+    rand bit rd_en;
+    rand bit [`DATA_WIDTH-1:0] data_in;
+    
+    // Output signals
+    bit [`DATA_WIDTH-1:0] data_out;
+    bit full;
+    bit empty;
+
+    // Class constructor
+    function new(string name);
+        super.new(name);
+    endfunction
+
+endclass
