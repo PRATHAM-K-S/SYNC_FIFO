af68c680e0ae0a192c4d22b23f57e4af9f4cf563
Pratham K S <prathamks2302@gmail.com>
2026-08-07 01:06:44 +0530

    testbench(sequencer): fixed syntax errors

--- testbench/sequencer/sync_fifo_seqr.sv
+++ testbench/sequencer/sync_fifo_seqr.sv
@@ -5,7 +5,7 @@ class sync_fifo_seqr extends uvm_sequencer #(sync_fifo_seq_item);
 
     // Class constructor    
     function new(string name="sync_fifo_seqr", uvm_component parent);
-        super.new(name, parent)
+        super.new(name, parent);
     endfunction
 
 endclass //sync_fifo_seqr extends uvm_sequencer #(sync_fifo_seq_item)
