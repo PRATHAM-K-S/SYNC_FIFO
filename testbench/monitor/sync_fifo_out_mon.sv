418992994237aed0b74578f3fd129a55b5c596a1
Pratham K S <prathamks2302@gmail.com>
2026-08-06 22:34:13 +0530

    testbench(monitor): added vif handle in sync_fifo_out_mon.sv

--- testbench/monitor/sync_fifo_out_mon.sv
+++ testbench/monitor/sync_fifo_out_mon.sv
@@ -3,6 +3,9 @@ class sync_fifo_out_mon extends uvm_monitor;
     // Factory registration
     `uvm_component_utils(sync_fifo_out_mon)
 
+    // Handle declerations
+    virtual sync_fifo_if.OUT_MON vif;
+
     // Class constructor
     function new(string name= "sync_fifo_out_mon", uvm_component parent);
         super.new(name, parent);
