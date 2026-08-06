5246b10d93af7e5c46fa1e1290dfa2ca7cf08833
Pratham K S <prathamks2302@gmail.com>
2026-08-06 12:03:01 +0530

    testbench(driver): added missing parameter to sync_fifo_driver class

--- testbench/driver/sync_fifo_driver.sv
+++ testbench/driver/sync_fifo_driver.sv
@@ -1,4 +1,4 @@
-class sync_fifo_driver extends uvm_driver;
+class sync_fifo_driver extends uvm_driver #(sync_fifo_seq_item);
 
     // Factory registration
     `uvm_component_utils(sync_fifo_driver)
@@ -7,5 +7,5 @@ class sync_fifo_driver extends uvm_driver;
     function new(string name="sync_fifo_driver", uvm_component parent);
         super.new(name, parent);
     endfunction
-    
+
 endclass
