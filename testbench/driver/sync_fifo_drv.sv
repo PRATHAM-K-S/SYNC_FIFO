be177e36c9e19217e65c9cfbdd56ce4bd0933148
Pratham K S <prathamks2302@gmail.com>
2026-08-06 22:18:42 +0530

    testbench(driver): added vf handle in sync_fifo_drv.sv

--- testbench/driver/sync_fifo_drv.sv
+++ testbench/driver/sync_fifo_drv.sv
@@ -3,6 +3,9 @@ class sync_fifo_drv extends uvm_driver #(sync_fifo_seq_item);
     // Factory registration
     `uvm_component_utils(sync_fifo_drv)
 
+    // Handle declerations
+    virtual sync_fifo_if.DRV vif;
+
     // Class constructor
     function new(string name="sync_fifo_drv", uvm_component parent);
         super.new(name, parent);
