6ed67e47955397c2887cb52c17d781c54cbc98e4
Pratham K S <prathamks2302@gmail.com>
2026-08-07 11:58:39 +0530

    testbench(coverage): added sync_fifo_cov.sv

---
+++ testbench/coverage/sync_fifo_cov.sv
@@ -0,0 +1,11 @@
+class sync_fifo_cov extends uvm_subscriber #(sync_fifo_seq_item);
+
+    // Factory registration
+    `uvm_component_utils(sync_fifo_cov)
+
+    // Class constructor
+    function new(string name="sync_fifo_cov", uvm_component parent);
+        super.new(name, this);
+    endfunction 
+
+endclass //sync_fifo_cov extends uvm_subscriber #(sync_fifo_seq_item)
