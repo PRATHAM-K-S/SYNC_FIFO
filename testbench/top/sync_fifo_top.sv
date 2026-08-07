a7c5f5a135f0ae06c4f65652a7af642094cbb3eb
Pratham K S <prathamks2302@gmail.com>
2026-08-07 22:30:14 +0530

    testbench(test): added clock generator in sync_fifo_top.sv

--- testbench/top/sync_fifo_top.sv
+++ testbench/top/sync_fifo_top.sv
@@ -15,9 +15,17 @@ module sync_fifo_top;
     // Interface decleration
     sync_fifo_if duv_if(clk, rst);
 
+    // Config and run_test
     initial begin
         uvm_config_db #(virtual sync_fifo_if)::set(null, "uvm_test_top", "vif", duv_if); //vif config
         run_test("sync_fifo_test"); //run_test
+    end
+
+    // Clock generator
+    initial begin
+        forever begin
+            #5 clk = ~clk;
+        end
     end
 
 endmodule
