cdda49e2c8b560f3db91378482ff3d699a038cc2
Pratham K S <prathamks2302@gmail.com>
2026-08-07 12:21:09 +0530

    testbench(scoreboard): fixed syntax errors in sync_fifo_scr.sv

--- testbench/scoreboard/sync_fifo_scr.sv
+++ testbench/scoreboard/sync_fifo_scr.sv
@@ -5,7 +5,7 @@ class sync_fifo_scr extends uvm_scoreboard;
 
     // Class construction
     function new(string name="sync_fifo_scr", uvm_component parent);
-        super.new(name, parent)
+        super.new(name, parent);
     endfunction
 
 endclass //sync_fifo_scr extends uvm_scoreboard
