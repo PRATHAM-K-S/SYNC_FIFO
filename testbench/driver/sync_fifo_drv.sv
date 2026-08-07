66e4699c3ecd004bc76a7c6bb01d487a225e90b9
Pratham K S <prathamks2302@gmail.com>
2026-08-07 20:37:17 +0530

    testbench(driver): added run_phase task and drive_item method in sync_fifo_drv.sv

--- testbench/driver/sync_fifo_drv.sv
+++ testbench/driver/sync_fifo_drv.sv
@@ -5,10 +5,31 @@ class sync_fifo_drv extends uvm_driver #(sync_fifo_seq_item);
 
     // Handle declerations
     virtual sync_fifo_if.DRV vif;
+    sync_fifo_seq_item req_item;
 
     // Class constructor
     function new(string name="sync_fifo_drv", uvm_component parent);
         super.new(name, parent);
     endfunction
+
+    // Run phase
+    task run_phase(uvm_phase phase);
+        forever begin
+            seq_item_port.get_next_item(req_item);
+            drive_item(req_item);
+            seq_item_port.item_done();
+        end
+    endtask
+
+    // Drive item
+    task drive_item(input sync_fifo_seq_item req_item);
+        `uvm_info("DRIVER", req_item.convert2String(), UVM_NONE);
+        @(vif.drv_cb);
+        vif.drv_cb.wr_cs <= req_item.wr_cs;
+        vif.drv_cb.rd_cs <= req_item.rd_cs;
+        vif.drv_cb.wr_en <= req_item.wr_en;
+        vif.drv_cb.rd_en <= req_item.rd_en;
+        vif.drv_cb.data_in <= req_item.data_in;
+    endtask
 
 endclass
