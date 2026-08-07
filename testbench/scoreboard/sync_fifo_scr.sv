1eca40881c49fd9d91c83a8e20bfb5955c35270c
Pratham K S <prathamks2302@gmail.com>
2026-08-07 11:18:13 +0530

    testbench(scoreboard): added sync_fifo_scr.sv

---
+++ testbench/scoreboard/sync_fifo_scr.sv
@@ -0,0 +1,11 @@
+class sync_fifo_scr extends uvm_scoreboard;
+
+    // Factory registration
+    `uvm_component_utils(sync_fifo_scr)
+
+    // Class construction
+    function new(string name="sync_fifo_scr", uvm_component parent);
+        super.new(name, parent)
+    endfunction
+
+endclass //sync_fifo_scr extends uvm_scoreboard
