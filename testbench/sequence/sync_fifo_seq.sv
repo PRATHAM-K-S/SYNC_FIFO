d4740d3cba1e11bbed0a1e481521ac72a1eb815a
Pratham K S <prathamks2302@gmail.com>
2026-08-07 22:25:48 +0530

    testbench(sequence): added seq_item handle in sync_fifo_seq.sv

--- testbench/sequence/sync_fifo_seq.sv
+++ testbench/sequence/sync_fifo_seq.sv
@@ -3,17 +3,20 @@ class sync_fifo_seq extends uvm_sequence #(sync_fifo_seq_item);
     // Factory registration
     `uvm_object_utils(sync_fifo_seq)
 
+    // Handle decleration
+    sync_fifo_seq_item seq_item;
+
     // Class constructor
     function new(string name="sync_fifo_seq");
-        super.new(name)
+        super.new(name);
     endfunction 
 
     task body();
-        req = sync_fifo_seq_item::type_id::create("req");
+        seq_item = sync_fifo_seq_item::type_id::create("seq_item");
         repeat(10) begin
-            start_item(req);
-            assert(req.randomize());
-            finish_item(req);
+            start_item(seq_item);
+            assert(seq_item.randomize());
+            finish_item(seq_item);
         end
     endtask
 
