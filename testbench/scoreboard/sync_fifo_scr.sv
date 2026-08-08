92e4eccdcc0215b73a876d76b5f17a6be37f744a
Pratham K S <prathamks2302@gmail.com>
2026-08-09 01:45:36 +0530

    testbench(scoreboard): added build_phase and connect_phase methods in sync_fifo_scr.sv

--- testbench/scoreboard/sync_fifo_scr.sv
+++ testbench/scoreboard/sync_fifo_scr.sv
@@ -3,9 +3,24 @@ class sync_fifo_scr extends uvm_scoreboard;
     // Factory registration
     `uvm_component_utils(sync_fifo_scr)
 
+    // Handle decleration
+    sync_fifo_pred pred;
+    uvm_analysis_export #(sync_fifo_seq_item) pred_axp;
+
     // Class construction
     function new(string name="sync_fifo_scr", uvm_component parent);
         super.new(name, parent);
+    endfunction
+
+    // Build phase
+    function void build_phase(uvm_phase phase);
+        pred = sync_fifo_pred::type_id::create("pred", this);
+        pred_axp = new("pred_axp", this);
+    endfunction
+
+    // Connect Phase
+    function void connect_phase(uvm_phase phase);
+        pred_axp.connect(pred.analysis_export);
     endfunction
 
 endclass //sync_fifo_scr extends uvm_scoreboard
