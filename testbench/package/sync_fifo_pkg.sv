4ca66e1bd704c121d3869efe06bb39ea505e8bde
Pratham K S <prathamks2302@gmail.com>
2026-08-08 13:50:31 +0530

    testbench(package): included sync_fifo_burst_write.sv

---
+++ testbench/package/sync_fifo_pkg.sv
@@ -0,0 +1,20 @@
+package sync_fifo_pkg;
+
+    import uvm_pkg::*;
+    `include "uvm_macros.svh"
+
+    `include "../sequence/sync_fifo_seq_item.sv"
+    `include "../config/sync_fifo_agt_cfg.sv"
+    `include "../driver/sync_fifo_drv.sv"
+    `include "../monitor/sync_fifo_inp_mon.sv"
+    `include "../monitor/sync_fifo_out_mon.sv"
+    `include "../sequencer/sync_fifo_seqr.sv"
+    `include "../agent/sync_fifo_agt.sv"
+    `include "../scoreboard/sync_fifo_scr.sv"
+    `include "../coverage/sync_fifo_cov.sv"
+    `include "../environment/sync_fifo_env.sv"
+    `include "../sequence/sync_fifo_seq.sv"
+    `include "../sequence/sync_fifo_burst_write_seq.sv"
+    `include "../test/sync_fifo_test.sv"
+    
+endpackage
